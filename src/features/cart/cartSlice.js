f48ca626e421834ef5ac6993c3d6a0c8309d4180
CongAnhCuteUwU <106415345+Code031@users.noreply.github.com>
2025-06-12 16:06:06 +0700

    first commit message

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,72 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+  //   cart: [
+  //     {
+  //       pizzaId: 12,
+  //       name: "Mediterranean",
+  //       quantity: 2,
+  //       unitPice: 16,
+  //       totalPrice: 32,
+  //     },
+  //   ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      // payload = newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      // payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (item) {
+        item.quantity += 1;
+        item.totalPrice = item.quantity * item.unitPice;
+      }
+    },
+    decreaseItemQuantity(state, action) {
+      // payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+        item.totalPrice = item.quantity * item.unitPice;
+      }
+    },
+    clearCart(state) {
+      // Clear the cart
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((total, item) => total + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((total, item) => total + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (pizzaId) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === pizzaId)?.quantity || 0;
+
+//reselect thư viện giúp giảm số lần tính toán lại giá trị khi state không thay đổi
---
+++ src/ui/Button.jsx
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+export default function Button({ children, disabled, to, type, onClick }) {
+  const base =
+    "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+
+  const style = {
+    primary: base + " px-4 py-3 sm:px-6 sm:py-4",
+    small: base + " px-2 py-1 text-sm sm:px-4 sm:py-2",
+    secondary:
+      base +
+      " px-4 py-3 sm:px-6 sm:py-4 bg-stone-200 text-stone-600 hover:bg-stone-300 focus:bg-stone-300 focus:ring-stone-300",
+  };
+
+  if (to) {
+    return (
+      <Link to={to} className={style[type]}>
+        {children}
+      </Link>
+    );
+  }
+
+  if (onClick) {
+    return (
+      <button onClick={onClick} disabled={disabled} className={style[type]}>
+        {children}
+      </button>
+    );
+  }
+
+  return (
+    <button disabled={disabled} className={style[type]}>
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool,
+  to: PropTypes.string,
+  type: PropTypes.oneOf(["primary", "small", "secondary"]),
+  onClick: PropTypes.func,
+};
